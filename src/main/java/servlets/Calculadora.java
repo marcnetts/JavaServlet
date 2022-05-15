caa59029ddfa9a834af8b2de5797996687284958
Marcos Zanetti <62448302+marcnetts@users.noreply.github.com>
2022-05-14 23:46:57 -0300

    Melhorada lógica caso sem operação

--- src/main/java/servlets/Calculadora.java
+++ src/main/java/servlets/Calculadora.java
@@ -37,21 +37,6 @@ public class Calculadora extends HttpServlet {
             String operacao = request.getParameter("operacao");
             Calculo calculo = null;
             
-            switch(operacao){
-                case "+":
-                    calculo = new Adicao();
-                    break;
-                case "-":
-                    calculo = new Subtracao();
-                    break;
-                case "*":
-                    calculo = new Multiplicacao();
-                    break;
-                case "/":
-                    calculo = new Divisao();
-                    break;
-            }
-            
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
@@ -62,19 +47,38 @@ public class Calculadora extends HttpServlet {
             out.println("   <h2>ADS - ELE2 - 5o. matutino</h2>");
             out.println("   <h4>Calculadora JSP</h4>");
             out.println("   ");
-            try{
-                calculo.setValor1(Float.valueOf(request.getParameter("numero1")));
-                calculo.setValor2(Float.valueOf(request.getParameter("numero2")));
+            if(operacao != null){
+                switch(operacao){
+                    case "+":
+                        calculo = new Adicao();
+                        break;
+                    case "-":
+                        calculo = new Subtracao();
+                        break;
+                    case "*":
+                        calculo = new Multiplicacao();
+                        break;
+                    case "/":
+                        calculo = new Divisao();
+                        break;
+                    default:
+                        break;
+                }
+
                 try{
-                    calculo.calcular();
-                    out.println(String.format("   <span>Numeros passados: %f %s %f = %f</span><br>", calculo.getValor1(), operacao, calculo.getValor2(), calculo.getResultado()));
+                    calculo.setValor1(Float.valueOf(request.getParameter("numero1")));
+                    calculo.setValor2(Float.valueOf(request.getParameter("numero2")));
+                    try{
+                        calculo.calcular();
+                        out.println(String.format("   <span>Numeros passados: %f %s %f = %f</span><br>", calculo.getValor1(), operacao, calculo.getValor2(), calculo.getResultado()));
+                    }
+                    catch(Exception e){
+                        out.println(String.format("   <span>Numeros passados: %f %s %f = Divisao por zero!</span><br>", calculo.getValor1(), operacao, calculo.getValor2()));
+                    }
                 }
                 catch(Exception e){
-                    out.println(String.format("   <span>Numeros passados: %f %s %f = Divisao por zero!</span><br>", calculo.getValor1(), operacao, calculo.getValor2()));
+                    out.println("   <span>Um ou mais valores passados estão incorretos! Passe apenas números.</span><br>");
                 }
-            }
-            catch(Exception e){
-                out.println("   <span>Um ou mais valores passados estão incorretos! Passe apenas números.</span><br>");
             }
             out.println("<br>");
             out.println("   ");
